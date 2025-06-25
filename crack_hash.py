dbd01d2674b7a4a0898e3f713f54633e3cf2d53f
aleyhany9 <aleyhany9@gmail.com>
2025-06-25 12:48:47 +0300

    CTF Toolkit, first tool is the hash cracker that name is crack_hash

---
+++ crack_hash.py
@@ -0,0 +1,34 @@
+import typer
+import hashlib
+
+
+def crack_hash(
+    hash_value: str = typer.Option(..., "--hash", help="Hash to crack"),
+    wordlist: str = typer.Option(..., "--wordlist", help="Path to wordlist file"),
+    algo: str = typer.Option("sha256", "--algo", help="Hash algorithm (md5, sha1, sha256)")
+):
+
+    try:
+        with open(wordlist, 'r', encoding='utf-8', errors='ignore') as f:
+            words = f.readlines()
+    except FileNotFoundError:
+        typer.echo(f"[!] Wordlist not found: {wordlist}")
+        raise typer.Exit()
+
+    hash_func = getattr(hashlib, algo, None)
+    if not hash_func:
+        typer.echo(f"[!] Unsupported algorithm: {algo}")
+        raise typer.Exit()
+
+    typer.echo(f"[*] Cracking using {algo.upper()}...")
+
+    for word in words:
+        word = word.strip()
+        if hash_func(word.encode()).hexdigest() == hash_value:
+            typer.echo(f"[+] Hash cracked! Plaintext: {word}")
+            return
+
+    typer.echo("[-] Failed to crack the hash.")
+
+if __name__ == "__main__":
+    typer.run(crack_hash)
